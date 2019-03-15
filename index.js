96714c312489ea8f46eee3188c9b685f3721c92b
Zachary Lehmann <zlehmann@gmail.com>
2019-03-15 00:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function driversWithRevenueOver(drivers, value) {
   const result = drivers.filter(driver => driver.revenue > value);
   return result;
 }
+
+function driverNamesWithRevenueOver(drivers, value) {
+  const targetDrivers = drivers.filter(driver => driver.revenue > value);
+  let result = [];
+  for (const driver of result)
+}
